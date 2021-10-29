29871b64b5938c70e3c94ce07dac000b1a08103a
Heather <78564780+heatherbenton@users.noreply.github.com>
2021-10-29 12:03:07 -0400

    Add files via upload
    uncomment server file

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 // install express
-//const express = require("express");
+const express = require("express");
 
 // connect to mongo config file
-//const connectDB = require("./config/db");
+const connectDB = require("./config/db");
 
-//const app = express();
+const app = express();
 
 // connect database
 connectDB();
 
 // init middleware
-//app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }));
 
 // get request
-//app.get("/", (req, res) => res.send("API running"));
+app.get("/", (req, res) => res.send("API running"));
 
 // define routes, connects to slash from router.get
-//app.use("/api/users", require("./routes/api/users"));
-//app.use("/api/auth", require("./routes/api/auth"));
-//app.use("/api/posts", require("./routes/api/posts"));
-//app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 
-//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-//app.listen(PORT, () => console.log(`Server stared on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server stared on port ${PORT}`));

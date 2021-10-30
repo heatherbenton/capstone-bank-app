67e4daaf0b2a703a6c383771041ddf3369ca5559
Heather <78564780+heatherbenton@users.noreply.github.com>
2021-10-30 19:13:55 -0400

    Add files via upload
    fix broken issues

--- mongo_test.js
+++ mongo_test.js
@@ -3,30 +3,30 @@ const url = "mongodb://localhost:27017";
 
 // connect to mongo
 MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-	console.log("Connected successfully to Mongo server");
+  console.log("Connected successfully to server");
 
-	// database Name
-	const dbName = "BankCapstone";
-	const db = client.db(dbName);
+  // database Name
+  const dbName = "myproject";
+  const db = client.db(dbName);
 
-	// new user
-	var name = "user" + Math.floor(Math.random() * 10000);
-	var email = name + "@mit.edu";
+  // new user
+  var name = "user" + Math.floor(Math.random() * 10000);
+  var email = name + "@mit.edu";
 
-	// insert into customer table
-	var collection = db.collection("customers");
-	var doc = { name, email };
-	collection.insertOne(doc, { w: 1 }, function (err, result) {
-		console.log("Document insert");
-	});
+  // insert into customer table
+  var collection = db.collection("customers");
+  var doc = { name, email };
+  collection.insertOne(doc, { w: 1 }, function (err, result) {
+    console.log("Document insert");
+  });
 
-	var customers = db
-		.collection("customers")
-		.find()
-		.toArray(function (err, docs) {
-			console.log("Collection:", docs);
+  var customers = db
+    .collection("customers")
+    .find()
+    .toArray(function (err, docs) {
+      console.log("Collection:", docs);
 
-			// clean up
-			client.close();
-		});
+      // clean up
+      client.close();
+    });
 });

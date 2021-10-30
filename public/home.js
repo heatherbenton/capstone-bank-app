ce01c652b7da2c2f7cd3a665db2938be39b1e907
Heather <78564780+heatherbenton@users.noreply.github.com>
2021-10-30 19:16:59 -0400

    Add files via upload
    fix frontend

--- public/home.js
+++ public/home.js
@@ -1,11 +1,11 @@
 function Home() {
-	return (
-		<Card
-			txtcolor="black"
-			header="Third Coast Bank"
-			title="welcome to our page"
-			text="may you navigate our site as easily as the waves rolling in"
-			body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
-		/>
-	);
+  return (
+    <Card
+      txtcolor="black"
+      header="Third Coast Bank"
+      title="welcome to our page"
+      text="may you navigate our site as easily as the waves rolling in"
+      body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
+    />
+  );
 }

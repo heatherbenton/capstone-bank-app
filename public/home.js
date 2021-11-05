72758281bb01d03b67c7932f49c9cc6263636352
Heather <78564780+heatherbenton@users.noreply.github.com>
2021-11-05 19:38:17 -0400

    modify content and clean that code up!

--- public/home.js
+++ public/home.js
@@ -1,11 +1,11 @@
 function Home() {
-  return (
-    <Card
-      txtcolor="black"
-      header="Third Coast Bank"
-      title="welcome to our page"
-      text="may you navigate our site as easily as the waves rolling in"
-      body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
-    />
-  );
+	return (
+		<Card
+			txtcolor="black"
+			header="Third Coast Bank"
+			title="the banking solution for the freshest coast around"
+			text="we make banking as relaxing as the rolling waves"
+			body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
+		/>
+	);
 }

f58cf8b07c629b06b05e259ddfea690287e64606
Heather <78564780+heatherbenton@users.noreply.github.com>
2021-11-05 19:37:06 -0400

    cleaner formatting on deposit page

--- public/deposit.js
+++ public/deposit.js
@@ -1,82 +1,70 @@
-function Deposit() {
-	const [show, setShow] = React.useState(true);
-	const [status, setStatus] = React.useState("");
+function Deposit(){
+  const [show, setShow]     = React.useState(true);
+  const [status, setStatus] = React.useState('');  
 
-	return (
-		<Card
-			bgcolor="info"
-			header="Deposit"
-			status={status}
-			body={
-				show ? (
-					<DepositForm setShow={setShow} setStatus={setStatus} />
-				) : (
-					<DepositMsg setShow={setShow} />
-				)
-			}
-		/>
-	);
+  return (
+    <Card
+      bgcolor="warning"
+      header="Deposit"
+      status={status}
+      body={show ? 
+        <DepositForm setShow={setShow} setStatus={setStatus}/> :
+        <DepositMsg setShow={setShow} setStatus={setStatus}/>}
+    />
+  )
 }
 
-function DepositMsg(props) {
-	return (
-		<>
-			<h5>Success</h5>
-			<button
-				type="submit"
-				className="btn btn-light"
-				onClick={() => props.setShow(true)}
-			>
-				Deposit again
-			</button>
-		</>
-	);
-}
+function DepositMsg(props){
+  return (<>
+    <h5>Success</h5>
+    <button type="submit" 
+      className="btn btn-light" 
+      onClick={() => {
+          props.setShow(true);
+          props.setStatus('');
+      }}>
+        Deposit again
+    </button>
+  </>);
+} 
 
-function DepositForm(props) {
-	const [email, setEmail] = React.useState("");
-	const [amount, setAmount] = React.useState("");
-	const ctx = React.useContext(UserContext);
+function DepositForm(props){
+  const [email, setEmail]   = React.useState('');
+  const [amount, setAmount] = React.useState('');
 
-	function handle() {
-		console.log(email, amount);
-		const user = ctx.users.find((user) => user.email == email);
-		if (!user) {
-			props.setStatus("fail!");
-			return;
-		}
+  function handle(){
+    fetch(`/account/update/${email}/${amount}`)
+    .then(response => response.text())
+    .then(text => {
+        try {
+            const data = JSON.parse(text);
+            props.setStatus(JSON.stringify(data.value));
+            props.setShow(false);
+            console.log('JSON:', data);
+        } catch(err) {
+            props.setStatus('Deposit failed')
+            console.log('err:', text);
+        }
+    });
+  }
 
-		user.balance = user.balance + Number(amount);
-		console.log(user);
-		props.setStatus("");
-		props.setShow(false);
-	}
+  return(<>
 
-	return (
-		<>
-			Email
-			<br />
-			<input
-				type="input"
-				className="form-control"
-				placeholder="Enter email"
-				value={email}
-				onChange={(e) => setEmail(e.currentTarget.value)}
-			/>
-			<br />
-			Amount
-			<br />
-			<input
-				type="number"
-				className="form-control"
-				placeholder="Enter amount"
-				value={amount}
-				onChange={(e) => setAmount(e.currentTarget.value)}
-			/>
-			<br />
-			<button type="submit" className="btn btn-light" onClick={handle}>
-				Deposit
-			</button>
-		</>
-	);
+    Email<br/>
+    <input type="input" 
+      className="form-control" 
+      placeholder="Enter email" 
+      value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+      
+    Amount<br/>
+    <input type="number" 
+      className="form-control" 
+      placeholder="Enter amount" 
+      value={amount} onChange={e => setAmount(e.currentTarget.value)}/><br/>
+
+    <button type="submit" 
+      className="btn btn-light" 
+      onClick={handle}>Deposit</button>
+
+  </>);
 }

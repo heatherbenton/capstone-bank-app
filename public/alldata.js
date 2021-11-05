6ae52cf5768a3ac9dc55814b595f1941f76a6b20
Heather <78564780+heatherbenton@users.noreply.github.com>
2021-11-05 19:53:43 -0400

    auth work in progress

--- public/alldata.js
+++ public/alldata.js
@@ -1,20 +1,22 @@
-function AllData(){
-    const [data, setData] = React.useState('');    
+function AllData() {
+	const [data, setData] = React.useState("");
 
-    React.useEffect(() => {
-        
-        // fetch all accounts from API
-        fetch('/account/all')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setData(JSON.stringify(data));                
-            });
+	React.useEffect(() => {
+		// fetch all accounts from API
+		fetch("/account/all")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setData(JSON.stringify(data));
+			});
+	}, []);
 
-    }, []);
+	// NEED AUTHORIZATION LOGIN HERE BEFORE SHOWING DATA!!!!!!!!!!
 
-    return (<>
-        <h5>All Data in Store:</h5>
-        {data}
-    </>);
+	return (
+		<>
+			<h5>Bank data:</h5>
+			{data}
+		</>
+	);
 }
